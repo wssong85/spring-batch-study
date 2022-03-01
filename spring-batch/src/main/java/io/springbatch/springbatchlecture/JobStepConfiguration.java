078864d528a13ad1e36322cd77cb8a20f78bb9e9
songwonseock <tablier@gmail.com>
2022-03-01 23:49:33 +0900

    jobExecution update

--- spring-batch/src/main/java/io/springbatch/springbatchlecture/JobStepConfiguration.java
+++ spring-batch/src/main/java/io/springbatch/springbatchlecture/JobStepConfiguration.java
@@ -3,7 +3,6 @@ package io.springbatch.springbatchlecture;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -11,19 +10,16 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Date;
-import java.util.Map;
-
 @Configuration
 @RequiredArgsConstructor
-public class JobExecutionConfiguration {
+public class JobStepConfiguration {
 
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
 
   @Bean
   public Job testJob() {
-    return jobBuilderFactory.get("testJob")
+    return jobBuilderFactory.get("sviu2Job")
             .start(Step1())
             .next(Step2())
             .build();
@@ -32,27 +28,14 @@ public class JobExecutionConfiguration {
   @Bean
   public Step Step1() {
     return stepBuilderFactory.get("step1")
-            .tasklet((contribution, chunkContext) -> {
-
-//              JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
-//              String name = jobParameters.getString("name");
-//              Long seq = jobParameters.getLong("seq");
-//              Date date = jobParameters.getDate("date");
-//              Double age = jobParameters.getDouble("age");
-//              Map<String, Object> parameters = chunkContext.getStepContext().getJobParameters();
-
-              System.out.println("step1 has executed");
-              return RepeatStatus.FINISHED;
-            }).build();
+            .tasklet(new CustomTasklet()).build();
   }
 
   @Bean
   public Step Step2() {
     return stepBuilderFactory.get("step2")
             .tasklet((contribution, chunkContext) -> {
-              System.out.println("step2 has executed");
-//              if (true)
-//              throw new RuntimeException("test1");
+              System.out.println("step2 is executed");
               return RepeatStatus.FINISHED;
             }).build();
   }
