269050515ca2929be8fca86271daa5ee971a626b
songwonseock <tablier@gmail.com>
2022-03-12 00:48:30 +0900

    update

--- spring-batch/src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
+++ spring-batch/src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,29 +19,73 @@ public class JobConfiguration {
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
 
+//  @Bean
+//  public Job batchJob1() {
+//    return jobBuilderFactory.get("batchJob1")
+//            .start(step1())
+//            .next(step2())
+//            .build();
+//  }
+
   @Bean
-  public Job thirdJob() {
-    return jobBuilderFactory.get("thirdJob")
-//            .preventRestart()
-            .start(thirdStep1())
-            .next(thirdStep2())
+  public Job batchJob2() {
+    return jobBuilderFactory.get("batchJob2")
+            .start(flow())
+            .next(step5())
+            .end()
             .build();
   }
 
   @Bean
-  public Step thirdStep1() {
-    return stepBuilderFactory.get("third_step1")
+  public Step step1() {
+    return stepBuilderFactory.get("step1")
             .tasklet((contribution, chunkContext) -> {
-              System.out.println("third app job step1");
+              System.out.println("step1");
               return RepeatStatus.FINISHED;
             }).build();
   }
 
   @Bean
-  public Step thirdStep2() {
-    return stepBuilderFactory.get("third_step2")
+  public Step step2() {
+    return stepBuilderFactory.get("step2")
             .tasklet((contribution, chunkContext) -> {
-              System.out.println("third app job step2");
+              System.out.println("step2");
+              return RepeatStatus.FINISHED;
+            }).build();
+  }
+  
+  @Bean
+  public Flow flow() {
+    FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+    flowBuilder.start(step3())
+            .next(step4())
+            .end();
+    return flowBuilder.build();
+  }
+
+  @Bean
+  public Step step3() {
+    return stepBuilderFactory.get("step3")
+            .tasklet((contribution, chunkContext) -> {
+              System.out.println("step3");
+              return RepeatStatus.FINISHED;
+            }).build();
+  }
+
+  @Bean
+  public Step step4() {
+    return stepBuilderFactory.get("step4")
+            .tasklet((contribution, chunkContext) -> {
+              System.out.println("step4");
+              return RepeatStatus.FINISHED;
+            }).build();
+  }
+
+  @Bean
+  public Step step5() {
+    return stepBuilderFactory.get("step5")
+            .tasklet((contribution, chunkContext) -> {
+              System.out.println("step5");
               return RepeatStatus.FINISHED;
             }).build();
   }
