64e1976e672d7de8b7f1675b54e75d639e2610d7
songwonseock <tablier@gmail.com>
2022-03-13 22:07:52 +0900

    update

--- spring-batch/src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
+++ spring-batch/src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
@@ -18,31 +18,36 @@ public class JobConfiguration {
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
 
-  @Bean
-  public Job batchJob() {
-    return jobBuilderFactory.get("batchJob")
-//            .incrementer(new CustomerJobParametersIncrementer())
-            .incrementer(new RunIdIncrementer())
+  private final String JOB_NAME = "JOB-";
+  private final String STEP_NAME = "STEP-";
+  private final String NUMBERING = "1";
+
+  @Bean(name = JOB_NAME + NUMBERING)
+  public Job job() {
+    return jobBuilderFactory.get(JOB_NAME + NUMBERING)
+//            .incrementer(new RunIdIncrementer())
             .start(step1())
             .next(step2())
             .build();
   }
 
-  @Bean
+  @Bean(name = STEP_NAME + NUMBERING + "-1")
   public Step step1() {
-    return stepBuilderFactory.get("step1")
+    return stepBuilderFactory.get(STEP_NAME + NUMBERING + "-1")
             .tasklet((contribution, chunkContext) -> {
-              System.out.println("execute step1");
+              System.out.println("step1 was executed");
               return RepeatStatus.FINISHED;
-            }).build();
+            })
+            .build();
   }
 
-  @Bean
+  @Bean(name = STEP_NAME + NUMBERING + "-2")
   public Step step2() {
-    return stepBuilderFactory.get("step2")
+    return stepBuilderFactory.get(STEP_NAME + NUMBERING + "-2")
             .tasklet((contribution, chunkContext) -> {
-              System.out.println("execute step2");
+              System.out.println("step2 was executed");
               return RepeatStatus.FINISHED;
-            }).build();
+            })
+            .build();
   }
 }
